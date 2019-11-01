148d5132e1f5ace762b8bb597bde336217d5f281
CodeeCodes <codeejpalmer@gmail.com>
2019-10-31 22:04:22 -0700

    Revised styling for next videos

--- sprint-1/src/components/mainContent/heroVideo.js
+++ sprint-1/src/components/mainContent/heroVideo.js
@@ -1,37 +1,10 @@
 import React from "react";
-import HeroImage from "../../assets/Images/hero-image.jpg";
-import PlayButton from "../../assets/Icons/svg/Icon-play.svg";
-import VolumeButton from "../../assets/Icons/svg/Icon-volume.svg";
-import FullButton from "../../assets/Icons/svg/Icon-fullscreen.svg";
-
+import HeroVideo from "../../assets/Video/heroVideo.mp4";
 
 export default function Hero(props) {
 	return (
 		<div className="hero">
-			<img src={HeroImage} alt="two bmx riders" className="hero__image" />
-			
-				<div className="hero__image-icons">
-					<div className="hero__image-icon-one">
-						<img
-							src={PlayButton}
-							alt="play button"
-							className="hero__image-icons-button"
-						/>
-					</div>
-					<div className="hero__image-icon-two">
-						<img
-							src={FullButton}
-							alt="full screen button"
-							className="hero__image-icons-button"
-						/>
-						<img
-							src={VolumeButton}
-							alt="volume button"
-							className="hero__image-icons-button"
-						/>
-					</div>
-				</div>
-	
+			<video src={HeroVideo} controls className="hero__video"></video>
 		</div>
 	);
 }
