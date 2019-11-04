cfdeb1180e0fc0d5c23dac24eb6da1420999413c
CodeeCodes <codeejpalmer@gmail.com>
2019-11-03 20:02:20 -0800

    Finished last stylings

--- sprint-1/src/components/header.js
+++ sprint-1/src/components/header.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Logo from "../assets/Logo/Logo-brainflix.svg";
 import avatar from "../assets/Images/Mohan-muruge.jpg";
-// import MagnifyLogo from "../assets/Icons/svg/Icon-search.svg";
 import PlusSign from "../assets/Icons/svg/Icon-upload.svg";
+
 export default function Header() {
 	return (
 		<header className="header">
@@ -14,13 +14,10 @@ export default function Header() {
 				/>
 			</div>
 			<div className="header__search-bar">
-				<input
-					type="search"
-					placeholder="Search"
-					className="search-bar-text"
-				></input>
+				<input type="search" placeholder="Search" className="search-bar-text">
+				</input>
 				<button className="header__search-button" type="submit">
-					<img src={PlusSign} alt="plus sign" className="plusSign"/>
+					<img src={PlusSign} alt="plus sign" className="plusSign" />
 					UPLOAD
 				</button>
 				<img src={avatar} alt="avatar" className="header__search-avatar" />
