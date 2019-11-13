603eef98902026df68a89f10bc5777099ebd5589
CodeeCodes <codeejpalmer@gmail.com>
2019-11-13 11:41:20 -0800

    working on file structure for sprint three and the backend side

---
+++ sprint-3/server/apiVideos.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+const fs = require("fs");
+
+const url = `https://project-2-api.herokuapp.com/videos?api_key=api_key=f7d8957a-1a18-4efc-b63a-22a6b2c06be0${process.argv[2]}`;
+console.log(process.argv);
+
+const videos = axios.get(url).then(response => {
+  fs.writeFile(
+    `${process.argv[2]}api/videos.json`,
+    JSON.stringify(response.data),
+    err => {
+      if (err) console.log(err);
+      console.log("file has been saved");
+    }
+  );
+});
+
+module.exports = videos;
--- sprint-3/server/index.js
+++ sprint-3/server/index.js
@@ -1,5 +1,4 @@
-const axios = require("axios");
-const fs = require("fs");
+const videos = require("./apiVideos");
 const path = require("path");
 const express = require("express");
 const app = express();
@@ -12,25 +11,11 @@ app.get("/api/videos", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
 
-app.use(
-  express.static(
-    "/Users/codeepalmer/Desktop/brainflix-codee-palmer/sprint-3/client/public/index.html"
-  )
-);
+// app.use(
+//   express.static(
+//     "/Users/codeepalmer/Desktop/brainflix-codee-palmer/sprint-3/client/public/index.html"
+//   )
+// );
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`port is running on ${PORT}`, __filename));
-
-// const url = `https://project-2-api.herokuapp.com/videos?api_key=api_key=f7d8957a-1a18-4efc-b63a-22a6b2c06be0${process.argv[2]}`;
-// console.log(process.argv);
-
-// axios.get(url).then(response => {
-//   fs.writeFile(
-//     `${process.argv[2]}-videos.json`,
-//     JSON.stringify(response.data),
-//     err => {
-//       if (err) console.log(err);
-//       console.log("file has been saved");
-//     }
-//   );
-// });
