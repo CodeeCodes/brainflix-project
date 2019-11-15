33a8746502e33a969904cd0aae8a260c174b0000
CodeeCodes <codeejpalmer@gmail.com>
2019-11-15 08:03:32 -0800

    POST functionality is working

--- sprint-3/server/routes/videos.js
+++ sprint-3/server/routes/videos.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
 const fileName =
   "/Users/codeepalmer/Desktop/brainflix-codee-palmer/sprint-3/server/model/sideVideos.json";
 let sideVideos = require(fileName);
@@ -9,8 +10,12 @@ let mainVideos = require(fileNameTwo);
 const logger = require("../middleware/logger");
 const app = express();
 const helper = require("../middleware/helper");
+const uuid = require("uuid");
 
 app.use(logger);
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 //  Get all videos
 router.get("/", (req, res) => {
   res.json(sideVideos);
@@ -18,8 +23,6 @@ router.get("/", (req, res) => {
 
 //  Get student with :id
 router.get("/:id", (req, res) => {
-  // res.send(req.params.id);
-
   const found = mainVideos.some(video => video.id === req.params.id);
 
   if (found) {
@@ -29,5 +32,24 @@ router.get("/:id", (req, res) => {
       .status(404)
       .json({ errorMessage: `Video with ID:${req.params.id} not found` });
   }
+});
+
+router.post("/", (req, res) => {
+  const newVideo = {
+    id: uuid.v4(),
+    title: req.body.title,
+    channel: req.body.channel,
+    image: req.body.image
+  };
+
+  if (!newVideo.channel) {
+    res
+      .status(400)
+      .json({ ErrorMessage: "You need to fill in all the felids" });
+  }
+  sideVideos.push(newVideo);
+  helper.writeJSONFile(fileName, sideVideos);
+  res.json(sideVideos);
+  res.redirect("/");
 });
 module.exports = router;
